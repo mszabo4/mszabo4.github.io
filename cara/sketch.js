df121713794f672da0dc3e06eb3d51f98ad702d6
mszabo4 <mszabo4@iespompeufabra.cat>
2024-09-24 14:11:54 +0200

    Update sketch.js

--- cara/sketch.js
+++ cara/sketch.js
@@ -12,23 +12,17 @@ function draw() {
   console.log(vulls)
   console.log(vol)
   background(127);
-  //Cara
   fill(227,192,139);
-  ellipse(300,200,250,300);
-  //Ull dret
-  fill(255);
-  ellipse(250,150,50,50);
-  fill(84,42,14);
+  ellipse(300,200,350,300);
+  fill(19,293,29);
+  ellipse(250,150,30,50);
+  fill(200,42,14);
   ellipse(250,150,10*sin(frameCount*0.1)+15,15);
-  //Ull esquerre
-  fill(255);
-  ellipse(350,150,50,50);
-  fill(84,42,14);
+  fill(190,29,389);
+  ellipse(350,150,30,50);
+  fill(200,42,14);
   ellipse(350,150,10*cos(frameCount*0.1)+15,15);
-  //Boca
   fill(200,15,15);
-  arc(300,250,100,10+vboca,0,PI,CHORD);
-  //Nas
-  fill(219,169,116)
-  triangle(300,200,300,225,275-vulls,212.5);
+  arc(300,250,200,3+vboca,0,PI,CHORD);
+ 
 }
