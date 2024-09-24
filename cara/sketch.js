74e9e3b077090c14e77cd20d92b879efe0a529aa
mszabo4 <mszabo4@iespompeufabra.cat>
2024-09-24 14:06:06 +0200

    Update sketch.js

--- cara/sketch.js
+++ cara/sketch.js
@@ -1,32 +1,34 @@
-let mic; //creem variable global  per un micròfon, ara es un espai a la memòria amb un nom
-function setup() {
- mic = new p5.AudioIn(); // La variable mic es una instancia de la classe "entrada de audio"(AudioIn)
- // Hem creat un nou objecte de la classe "entrada de audio" i tindrà totes les seves característiques i s'anomena "mic"
- createCanvas(600,400);
- // put setup code here
+function setup() 
+{
+  createCanvas(600,400);
+  mic=new p5.AudioIn();
+  mic.start();
 }
 
 function draw() {
- let vol = mic.getLevel();  //Apliquem el mètode "getLevel" que ens donarà el nivell de volum entre 0 i 1 
- //Registrat per l'objecte mic, que és una entrada de àudio. Aquest número el guardarem en una variable local
- //(Perque afecta només al draw a diferència de la variable global mic que està al principi i afecta a tot el codi.
- // El punt que hi ha entre mic i getLevel és molt important, s'anomena la sintaxi del punt o "dot syntax". Aquest punt aplica mètodes a objectes.
- // És un sistema que et permet crear qualsevol cosa i que faci qualsevol cosa.
- let h = map(vol,0,1,0,300); // "map" és una funció molt potent què fa moltes coses.
- // El que fa es convertir els números que estan entre 0 i 1, sempre en una escala proporcional o mapejada.
- // entre els números 0 i 300, vol dir que si el volum fos 0,5, es cconverteix en 150 i s'emagatzema en "h"
- // Si el número es 0,33 de volum es convertirà en 100 de "H" o alçada de la boca o arc.
- // Si el número es 0,67 de volum es convertirà en 200 de "H" o alçada de la boca o arc.
- background(127);
- fill(310,190,196);
- ellipse(300,220,240,310,);
- fill(498,105,270);
- ellipse(250,150,60,30);
- ellipse(350,150,60,30);
- fill(255,0,0);
- arc(300,250,120,h,0,PI); // La variable "h" determina la alçada de la boca
- console.log(mic);
- console.log(vol);
- console.log(h);
-  //Els consoles.lo ens serveixen pe veure el valor de les variables en cada moment
+  let vol= mic.getLevel();
+  let vulls= map(vol,0,1,0,100);
+  let vboca= map(vol,0,1,0,300);
+  console.log(vulls)
+  console.log(vol)
+  background(127);
+  //Cara
+  fill(227,192,139);
+  ellipse(300,200,250,300);
+  //Ull dret
+  fill(255);
+  ellipse(250,150,50,50);
+  fill(84,42,14);
+  ellipse(250,150,10*sin(frameCount*0.1)+15,15);
+  //Ull esquerre
+  fill(255);
+  ellipse(350,150,50,50);
+  fill(84,42,14);
+  ellipse(350,150,10*cos(frameCount*0.1)+15,15);
+  //Boca
+  fill(200,15,15);
+  arc(300,250,100,10+vboca,0,PI,CHORD);
+  //Nas
+  fill(219,169,116)
+  triangle(300,200,300,225,275-vulls,212.5);
 }
