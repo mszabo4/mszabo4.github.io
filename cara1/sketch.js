0ff9f9e528287e3052ecff4c43f5123a88ed4e82
mszabo4 <mszabo4@iespompeufabra.cat>
2025-03-26 10:50:54 +0100

    Update sketch.js

--- cara1/sketch.js
+++ cara1/sketch.js
@@ -1,58 +1,28 @@
-let mic; 
-let bgColor;
-
-function setup() {
-    createCanvas(600, 400);
-    mic = new p5.AudioIn();
-    mic.start();
-    bgColor = color(200, 220, 255);
+function setup() 
+{
+  createCanvas(600,400);
+  mic=new p5.AudioIn();
+  mic.start();
 }
 
 function draw() {
-    let faceX = 300;
-    let faceY = 200;
-    let leftEyeSize = 30 + 10 * cos(frameCount * 0.1);
-    let rightEyeSize = 30 + 10 * sin(frameCount * 0.1);
-    let vol = mic.getLevel();
-    let h = map(vol, 0, 1, 0, 300);
-    let x = 200 * noise(0.1 * frameCount);
-    let y = 200 * noise(0.1 * frameCount + 100);
-    let ales = 10 * random(1);
-    
-    background(bgColor);
-    fill(255, 223, 196);
-    ellipse(faceX, faceY, 350, 270);
-    
-    // Ojos
-    fill(19, 23, 29);
-    ellipse(faceX - 50, faceY - 50, 50, leftEyeSize);
-    ellipse(faceX + 50, faceY - 50, 50, rightEyeSize);
-    
-    // Pupilas
-    fill(0);
-    let xeye = map(mouseX, 0, width, -10, 60);
-    let yeye = map(mouseY, 0, height, -10, 60);
-    ellipse(xeye - 50, yeye - 50, 25, leftEyeSize / 2);
-    ellipse(xeye + 50, yeye - 50, 25, rightEyeSize / 2);
-    
-    // Boca
-    fill(180, 15, 15);
-    arc(faceX, faceY + 50, 120, 3 + h, 0, PI, CHORD);
-    
-    // Mosca
-    fill(255);
-    noStroke();
-    ellipse(x - 10, y, 15 + ales, 25);
-    ellipse(x + 10, y, 15 + ales, 25);
-    fill(0);
-    ellipse(x, y, 10, 20);
-    
-    // Texto
-    fill(0);
-    textSize(12);
-    text("Cara con mosca", 50, 350);
-}
-
-function changeBackground() {
-    bgColor = color(random(100, 255), random(100, 255), random(100, 255));
+  let vol= mic.getLevel();
+  let vulls= map(vol,0,1,0,100);
+  let vboca= map(vol,0,1,0,300);
+  console.log(vulls)
+  console.log(vol)
+  background(127);
+  fill(227,192,139);
+  ellipse(300,200,350,300);
+  fill(19,293,29);
+  ellipse(210,150,30,50);
+  fill(200,42,14);
+  ellipse(210,150,10*sin(frameCount*0.1)+15,15);
+  fill(190,29,389);
+  ellipse(310,150,30,50);
+  fill(200,42,14);
+  ellipse(310,150,10*cos(frameCount*0.1)+15,15);
+  fill(200,15,15);
+  arc(300,250,200,3+vboca,0,PI,CHORD);
+ 
 }
